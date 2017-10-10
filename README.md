79f405a9ad5e64d8ef56a45635afe0db23202413
mau5atron <gabrielbetancourt96@outlook.com>
2017-10-10 01:02:20 -0400

    This is my first sinatra server

---
+++ README.md
@@ -0,0 +1,6 @@
+This is my first sinatra web server.
+The list of things I needed to create this server were:
+
+Gemfile - This specifies the requirements needed to run the software.
+Procfile - instructs other computer on how to run the program.
+Requiring 'sinatra' in my server.rb file
--- server.rb
+++ server.rb
@@ -1,16 +1,17 @@
-require 'sinatra' #sinatra is a micro framework
+	require 'sinatra' #sinatra is a micro framework
 
-get '/' do # web server root path
+# web server root path instead of /hello.txt
+get '/' do 
 	"Hello world"
 end
 
-get "/sinatra" do #
-	"Hello Sinatra"
-end
+# get "/sinatra" do #
+# 	"Hello Sinatra"
+# end
 
-get '/the_truth' do
-	"seek and ye shall find"
-end
+# get '/the_truth' do
+# 	"seek and ye shall find"
+# end
 
 #whatever changes that are made have to be 
 #saved and the server.rb file has to be run again.
