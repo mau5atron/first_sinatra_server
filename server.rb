0f3f993959a67a9406112235c6e1933362ee1b47
mau5atron <gabrielbetancourt96@outlook.com>
2017-10-30 00:29:22 -0400

    updated server.rb to reflect hello.txt contents being shown from the root of the localhost server

--- server.rb
+++ server.rb
@@ -2,16 +2,10 @@
 
 # web server root path instead of /hello.txt
 get '/' do 
-	"Hello world"
+	send_file File.join(settings.public_folder, 'hello.txt')
 end
 
-# get "/sinatra" do #
-# 	"Hello Sinatra"
-# end
 
-# get '/the_truth' do
-# 	"seek and ye shall find"
-# end
 
 #whatever changes that are made have to be 
 #saved and the server.rb file has to be run again.
